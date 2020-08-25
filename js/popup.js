8299868366c7d56abb4b0dbe83636bfec0797aa5
Tyler Lin <tylerhlin@gmail.com>
2020-08-25 14:24:54 -0400

    Change popup

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,30 @@
-fetch('../README.md')
-    .then((response) => response.text())
-    .then((text) => {
-        text = text.replace(/(\r\n|\r|\n)## .+/g, replacer)
-        text = text.replace('\n</details>\n', '')
-        text += '\n\n</details>'
-        console.log(text)
-        document.getElementById('markdown').innerHTML = marked(text)
-    })
+// fetch('../README.md')
+//     .then((response) => response.text())
+//     .then((text) => {
+//         text = text.replace(/(\r\n|\r|\n)## .+/g, replacer)
+//         text = text.replace('\n</details>\n', '')
+//         text += '\n\n</details>'
+//         console.log(text)
+//         document.getElementById('markdown').innerHTML = marked(text)
+//     })
 
-function replacer(match) {
-    const title = match.substring(5)
-    return `
-</details>
+// function replacer(match) {
+//     const title = match.substring(5)
+//     return `
+// </details>
 
-<details>
-    <summary style="font-size: 1.25em; font-weight: bold;">${title}</summary>`
+// <details>
+//     <summary style="font-size: 1.25em; font-weight: bold;">${title}</summary>`
+// }
+
+const MDCRipple = mdc.ripple.MDCRipple
+for (const button of document.getElementsByClassName('mdc-button')) {
+    new MDCRipple(button)
 }
+
+document.querySelector("#docs").addEventListener('click', function () {
+    chrome.tabs.create({ url: 'https://github.com/tytot/attendance-for-google-meet#attendance-for-google-meet' })
+})
+document.querySelector("#contact").addEventListener('click', function () {
+    chrome.tabs.create({ url: 'mailto:tyleradit@gmail.com?subject=Regarding%20the%20Attendance%20for%20Google%20Meet%20Chrome%20Extension' })
+})
